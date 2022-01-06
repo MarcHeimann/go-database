bee1d538c563e0afc000cebe8509bdfb731b0d42
Marc Heimann <Marc.Heimann@vanderlande.com>
2022-01-06 14:20:24 +0100

    CreateTable function awaits now an object of type map[string]interface{}

--- database.go
+++ database.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -59,13 +61,15 @@ func checkAndCreateDatabase() (string, error) {
 	return "Success", nil
 }
 
-func CreateTableForAggregate(def string) error {
+func CreateTableForAggregate(aggregateMap map[string]interface{}) error {
+
+	log.Info(aggregateMap)
+
 	conn, err := pool.Acquire(context.Background())
 	if err != nil {
 		log.Error(os.Stderr, "Error acquiring connection:", err)
 		os.Exit(1)
 	}
-	defer conn.Release()
 
 	log.Info("Creating database table")
 
@@ -75,5 +79,43 @@ func CreateTableForAggregate(def string) error {
 
 	log.Info(row)
 
+	defer conn.Release()
+
 	return nil
+}
+
+func ConvertStructToMap(st interface{}) map[string]interface{} {
+
+	reqRules := make(map[string]interface{})
+
+	v := reflect.ValueOf(st)
+	t := reflect.TypeOf(st)
+
+	for i := 0; i < v.NumField(); i++ {
+		key := strings.ToLower(t.Field(i).Name)
+		typ := v.FieldByName(t.Field(i).Name).Kind().String()
+		structTag := t.Field(i).Tag.Get("json")
+		jsonName := strings.TrimSpace(strings.Split(structTag, ",")[0])
+		value := v.FieldByName(t.Field(i).Name)
+
+		// if jsonName is not empty use it for the key
+		if jsonName != "" && jsonName != "-" {
+			key = jsonName
+		}
+
+		if typ == "string" {
+			if !(value.String() == "" && strings.Contains(structTag, "omitempty")) {
+				fmt.Println(key, value)
+				fmt.Println(key, value.String())
+				reqRules[key] = value.String()
+			}
+		} else if typ == "int" {
+			reqRules[key] = value.Int()
+		} else {
+			reqRules[key] = value.Interface()
+		}
+
+	}
+
+	return reqRules
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/MarcHeimann/go-database
 go 1.14
 
 require (
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/jackc/pgx/v4 v4.13.0
 	github.com/sirupsen/logrus v1.8.1
